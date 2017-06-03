eebf13f9cdb453b6d9bc244aa6dc187e53a0f823
bigspawn <bigspawn@yandex.ru>
2017-06-03 21:49:57 +0300

    Add normal parsing news

--- src/main/java/ru/bigspawn/parser/Main.java
+++ src/main/java/ru/bigspawn/parser/Main.java
@@ -4,12 +4,18 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URLEncoder;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by bigspawn on 30.05.2017.
@@ -17,7 +23,9 @@ import java.util.*;
 public class Main {
     private static ArrayList<News> newsArrayList = new ArrayList<>();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // TODO: 31.05.2017 добавлять все в базу
+        // TODO: 31.05.2017 обрабатывать логику повторяющихся статей
 //        try {
 //            Class.forName("org.h2.Driver");
 //            Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "admin", "52169248");
@@ -27,6 +35,8 @@ public class Main {
 //        } catch (ClassNotFoundException e) {
 //            e.printStackTrace();
 //        }
+
+        TimeUnit.SECONDS.sleep(20);
         int pageNumber = 1;
 
         WebClient client = new WebClient();
@@ -41,11 +51,12 @@ public class Main {
             for (HtmlElement element : elements) {
                 List<HtmlElement> titleTds = element.getElementsByAttribute("td", "class", "category");
                 if (titleTds != null && !titleTds.isEmpty()) {
-                    String newsCategory = titleTds.get(0).asText();
+                    String newsCategory = titleTds.get(0).asText().trim();
                     Optional<NewsType> optional = Arrays.stream(NewsType.values())
                                                         .filter(x -> newsCategory.equals(x.getName()))
                                                         .findFirst();
                     if (optional.isPresent()) {
+                        NewsType newsType = optional.get();
                         HtmlElement titleElement = element.getElementsByAttribute("td", "class", "ntitle").get(0);
                         String newsURL = titleElement.getElementsByTagName("a").get(0).getAttribute("href");
                         page = client.getPage(newsURL);
@@ -53,6 +64,7 @@ public class Main {
                         if (newsElements != null && !newsElements.isEmpty()) {
                             HtmlElement newsElement = newsElements.get(0);
                             String newsTitle = ((HtmlElement) newsElement.getByXPath("//table/tbody/tr/td/table/tbody/tr/td[@class=\"ntitle\"]").get(0)).getTextContent();
+
                             HtmlElement newsBodyHtmlElement = (HtmlElement) newsElement.getByXPath("//div[contains(@id, \"news-id\")]").get(0);
                             String newsBody = newsBodyHtmlElement.asText();
 
@@ -63,43 +75,64 @@ public class Main {
                             Optional<String> gender = lines.stream().filter(line -> StringUtils.containsIgnoreCase(line, "Стиль")).findFirst();
                             gender.ifPresent(newsGender::append);
 
+                            if (newsGender.length() == 0) {
+                                gender = lines.stream().filter(line -> StringUtils.containsIgnoreCase(line, "Жанр")).findFirst();
+                                gender.ifPresent(newsGender::append);
+                            }
+
                             StringBuilder newsCountry = new StringBuilder();
-                            Optional<String> country = lines.stream().filter(line -> StringUtils.containsIgnoreCase(line, "Страна")).findFirst();
-                            country.ifPresent(newsCountry::append);
-
                             StringBuilder newsFormat = new StringBuilder();
-                            Optional<String> format = lines.stream().filter(line -> StringUtils.containsIgnoreCase(line, "Формат")).findFirst();
-                            format.ifPresent(newsFormat::append);
+                            if (newsType != NewsType.Concerts) {
+                                Optional<String> country = lines.stream().filter(line -> StringUtils.containsIgnoreCase(line, "Страна")).findFirst();
+                                country.ifPresent(newsCountry::append);
+                                if (newsCountry.length() == 0) {
+                                    country = lines.stream().filter(line -> StringUtils.containsIgnoreCase(line, "Родина")).findFirst();
+                                    country.ifPresent(newsCountry::append);
+                                }
 
-                            StringBuilder tracks = new StringBuilder();
-                            for (int i = 0; i < lines.size(); i++) {
-                                if (StringUtils.contains(lines.get(i), "Треклист")) {
-                                    for (int j = i + 1; j < lines.size(); j++) {
-                                        String track = lines.get(j);
-                                        if (Character.isDigit(track.charAt(0))) {
-                                            tracks.append(track).append(System.lineSeparator());
-                                        }
-                                    }
-                                    break;
+                                Optional<String> format = lines.stream().filter(line -> StringUtils.containsIgnoreCase(line, "Формат")).findFirst();
+                                format.ifPresent(newsFormat::append);
+
+                                if (newsFormat.length() == 0) {
+                                    format = lines.stream().filter(line -> StringUtils.containsIgnoreCase(line, "Качество")).findFirst();
+                                    format.ifPresent(newsFormat::append);
                                 }
                             }
 
-                            String newsImageURL = "";
+                            String tracks = getTrackList(lines);
+
+                            String newsImageUrl = "";
                             String href = "";
                             List<HtmlElement> aElements = newsBodyHtmlElement.getElementsByTagName("a");
                             if (aElements != null && !aElements.isEmpty()) {
-                                List<HtmlElement> imageElements = aElements.get(0).getElementsByTagName("img");
-                                if (imageElements != null && !imageElements.isEmpty()) {
-                                    newsImageURL = imageElements.get(0).getAttribute("src");
-                                }
-                                HtmlElement downloadElement = aElements.get(aElements.size() - 1);
-                                href = downloadElement.getAttribute("href");
+                                newsImageUrl = getImageUrl(aElements);
+                                href = getHref(aElements);
+                            }
+                            if (newsImageUrl == null || newsImageUrl.isEmpty()) {
+                                newsImageUrl = getImageSrc(newsBodyHtmlElement.getElementsByTagName("img"));
                             }
 
+                            DateTimeFormatter formatter = DateTimeFormat.forPattern("dd MMMM yyyy");
+                            DateTime dateTime = DateTime.now();
+                            List<HtmlElement> commentElements = newsElement.getByXPath("//*[@id=\"dle-content\"]/table/tbody/tr/td/div[@class=\"slink1\"]");
+                            if (commentElements != null && !commentElements.isEmpty()) {
+                                String date = commentElements.get(0).getTextContent().trim();
+                                if (StringUtils.contains(date, "Вчера")) {
+                                    dateTime = dateTime.minusDays(1);
+                                } else if (!StringUtils.contains(date, "Сегодня")) {
+                                    Pattern pattern = Pattern.compile("\\|\\s.*");
+                                    Matcher matcher = pattern.matcher(date);
+                                    if (matcher.find()) {
+                                        date = matcher.group();
+                                        date = date.replace("|", "").trim();
+                                        dateTime = formatter.parseDateTime(date);
+                                    }
+                                }
+                            }
+                            System.out.println(formatter.print(dateTime));
 
                             System.out.println("Add news");
-                            News news = new News(newsTitle, newsCategory, null, null, null,
-                                    newsGender.toString(), newsFormat.toString(), newsCountry.toString(), tracks.toString(), href, newsImageURL);
+                            News news = new News(newsTitle, newsType, dateTime, newsGender.toString(), newsFormat.toString(), newsCountry.toString(), tracks, href, newsImageUrl);
                             System.out.println(news);
                             System.out.println("--------------------------------------------------------");
                             newsArrayList.add(news);
@@ -108,8 +141,55 @@ public class Main {
                     }
                 }
             }
+            TimeUnit.MINUTES.sleep(1);
         } catch (UnsupportedEncodingException | MalformedURLException e) {
             e.printStackTrace();
         }
+
+    }
+
+    private static String getTrackList(ArrayList<String> lines) {
+        StringBuilder tracks = new StringBuilder("");
+        for (int i = 0; i < lines.size(); i++) {
+            if (StringUtils.contains(lines.get(i), "Треклист") || StringUtils.contains(lines.get(i), "Tracklist")) {
+                for (int j = i + 1; j < lines.size(); j++) {
+                    String track = lines.get(j).trim();
+                    if (Character.isDigit(track.charAt(0))) {
+                        tracks.append(track);
+                        if (j < lines.size() - 1 && Character.isDigit(lines.get(j + 1).charAt(0))) {
+                            tracks.append("\n");
+                        } else {
+                            break;
+                        }
+                    }
+                }
+                break;
+            }
+        }
+        return tracks.toString();
+    }
+
+    private static String getImageUrl(List<HtmlElement> aElements) {
+        List<HtmlElement> imageElements = aElements.get(0).getElementsByTagName("img");
+        return getImageSrc(imageElements);
+    }
+
+    private static String getImageSrc(List<HtmlElement> imageElements) {
+        if (imageElements != null && !imageElements.isEmpty()) {
+            HtmlElement imageElement = imageElements.get(0);
+            if (imageElement != null) {
+                return imageElement.getAttribute("src");
+            }
+        }
+        return "";
+
+    }
+
+    private static String getHref(List<HtmlElement> elements) {
+        HtmlElement downloadElement = elements.get(elements.size() - 1);
+        if (downloadElement != null) {
+            return downloadElement.getAttribute("href");
+        }
+        return "";
     }
 }
--- src/main/java/ru/bigspawn/parser/News.java
+++ src/main/java/ru/bigspawn/parser/News.java
@@ -7,10 +7,8 @@ import org.joda.time.DateTime;
  */
 public class News {
     private String title;
-    private String category;
-    private String text;
+    private NewsType type;
     private DateTime dateTime;
-    private String author;
     private String genre;
     private String format;
     private String country;
@@ -18,18 +16,16 @@ public class News {
     private String downloadURL;
     private String imageURL;
 
-    public News(String title, String category) {
+    public News(String title, NewsType type) {
         this.title = title;
-        this.category = category;
+        this.type = type;
     }
 
-    public News(String title, String category, String text, DateTime dateTime, String author,
-                String genre, String format, String country, String playlist, String downloadURL, String imageURL) {
+    public News(String title, NewsType type, DateTime dateTime, String genre, String format,
+                String country, String playlist, String downloadURL, String imageURL) {
         this.title = title;
-        this.category = category;
-        this.text = text;
+        this.type = type;
         this.dateTime = dateTime;
-        this.author = author;
         this.genre = genre;
         this.format = format;
         this.country = country;
@@ -38,94 +34,12 @@ public class News {
         this.imageURL = imageURL;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public DateTime getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(DateTime dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public String getFormat() {
-        return format;
-    }
-
-    public void setFormat(String format) {
-        this.format = format;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getPlaylist() {
-        return playlist;
-    }
-
-    public void setPlaylist(String playlist) {
-        this.playlist = playlist;
-    }
-
-    public String getDownloadURL() {
-        return downloadURL;
-    }
-
-    public void setDownloadURL(String downloadURL) {
-        this.downloadURL = downloadURL;
-    }
-
     @Override
     public String toString() {
         return "News{" +
                 "title='" + title + '\'' +
-                ", category='" + category + '\'' +
-                ", text='" + text + '\'' +
+                ", type='" + type + '\'' +
                 ", dateTime=" + dateTime +
-                ", author='" + author + '\'' +
                 ", genre='" + genre + '\'' +
                 ", format='" + format + '\'' +
                 ", country='" + country + '\'' +
